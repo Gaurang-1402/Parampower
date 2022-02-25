40c914b4d491330e1a3c469046d508a7e4d6e0ac
Gaurang Ruparelia <gr2159@nyu.edu>
2022-02-25 18:07:18 -0500

    tailwind config complete

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      primary: "#DECAF1",
+      secondary: "#9259C4",
+      quarternary: "#8327D2",
+      tertiary: "#924AD2",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+  plugins: [],
+}
